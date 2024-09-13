bf73166430407d6b6eaaa6c5a4415420c5bea2ac
yuta <yasu.private.ct@gmail.com>
2024-09-13 11:48:05 +0900

    seed

--- theatre-siat-mark/prisma/seed.ts
+++ theatre-siat-mark/prisma/seed.ts
@@ -1,6 +1,7 @@
 import { theaters } from "../src/data/theaters";
 import { articles } from "../src/data/articles";
 import { PrismaClient } from "@prisma/client";
+import { screens } from "../src/data/screens";
 
 const prisma = new PrismaClient();
 
@@ -20,6 +21,21 @@ async function main() {
         id: theater.id,
         name: theater.name,
         address: theater.address,
+      },
+    });
+  }
+
+  for (const screen of screens){
+    await prisma.screen.upsert({
+      where: { id: screen.id },
+      update: {
+        name: screen.name,
+        theater_id: screen.theater_id,
+      },
+      create: {
+        id: screen.id,
+        name: screen.name,
+        theater_id: screen.theater_id,
       },
     });
   }
---
+++ theatre-siat-mark/src/data/screens.ts
@@ -0,0 +1,12 @@
+export const screens = [
+  {
+    id: 1,
+    theater_id: 1,
+    name: 'スクリーン1',
+  },
+  {
+    id: 2,
+    theater_id: 1,
+    name: 'スクリーン2',
+  },
+];
