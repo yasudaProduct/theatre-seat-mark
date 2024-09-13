9e24d7cf31d4997a8eedd076b195954a3ad8a5ca
yuta <yasu.private.ct@gmail.com>
2024-09-13 12:11:17 +0900

    review登録API

---
+++ theatre-siat-mark/src/pages/api/seat-reviews/index.ts
@@ -0,0 +1,41 @@
+import prisma from '@/lib/prisma'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { authOptions } from '../auth/[...nextauth]';
+import { getServerSession } from 'next-auth';
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (req.method === 'POST') {
+        const { screenId, seatNumber, review, rating, userId } = req.body
+
+        const session = await getServerSession(req, res, authOptions);
+        if (session?.user?.email) {
+
+            if (!screenId || !seatNumber || !review || !rating || !session?.user?.id) {
+                return res.status(400).json({ message: '必要な情報が不足しています' })
+            }
+
+            try {
+                const newReview = await prisma.seatReview.create({
+                    data: {
+                        screen_id: parseInt(screenId, 10),
+                        seat_name: seatNumber,
+                        review: review,
+                        //   rating: parseInt(rating, 10),
+                        user_id: parseInt(userId, 10)
+                    }
+                })
+                res.status(201).json(newReview)
+            } catch (error) {
+                res.status(500).json({ message: 'エラーが発生しました', error })
+            }
+        } else {
+            res.setHeader('Allow', ['POST'])
+            res.status(405).end(`Method ${req.method} Not Allowed`)
+        }
+    }else{
+        res.status(401).send({ message: 'Unauthorized' });
+    }
+}
