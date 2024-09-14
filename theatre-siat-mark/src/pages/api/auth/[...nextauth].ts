a4974bb0466b5f7ad64d7e89fd7c0b298c314e78
yasudaProduct <yasu.private.ct@gmail.com>
2024-09-14 09:39:38 +0900

    sessionにuser_id追加

--- theatre-siat-mark/src/pages/api/auth/[...nextauth].ts
+++ theatre-siat-mark/src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,15 @@
-import NextAuth, { Session } from 'next-auth';
-import { AdapterUser } from 'next-auth/adapters';
+import prisma from '@/lib/prisma';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+// import { NextApiHandler } from 'next';
+import NextAuth, { DefaultUser, Session } from 'next-auth';
+// import { AdapterUser } from 'next-auth/adapters';
 import GitHubProvider from 'next-auth/providers/github';
 
+// const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, authOptions);
+// export default authHandler;
+
 export const authOptions = {
+  adapter: PrismaAdapter(prisma),
   providers:[
     GitHubProvider({
       clientId: process.env.GITHUB_ID!,
@@ -10,15 +17,17 @@ export const authOptions = {
     }),
   ],
   callbacks: {
-    session: ({ session, user }: { session: Session, user: AdapterUser }) => ({
-      ...session,
-      user: {
-        ...user,
-        id: user.id,
+    async session({session, user}: {session: Session, user: DefaultUser}) {
+      if (session?.user) {
+        session.user.id = user.id;
       }
-    }),
+      return session;
+    },
   },
   secret: process.env.SECRET,
+  // pages: {
+  //   signIn: '/auth/login',
+  // },
 }
 
 export default NextAuth(authOptions)
--- theatre-siat-mark/src/types/next-auth.d.ts
+++ theatre-siat-mark/src/types/next-auth.d.ts
@@ -1,9 +1,9 @@
-import type { DefaultSession } from 'next-auth';
+import type { DefaultUser } from "next-auth";
 
 declare module 'next-auth' {
   interface Session {
-    user: {
+    user?: DefaultUser & {
       id: string;
-    } & DefaultSession['user'];
+    };
   }
 }
