07beddb9c542f5cd32e11dca315ec2868cc32d9f
yasudaProduct <yasu.private.ct@gmail.com>
2024-11-04 17:38:49 +0900

    EditPage表示修正

--- src/pages/reviews/edit/[id].tsx
+++ src/pages/reviews/edit/[id].tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
@@ -7,6 +7,9 @@ import { Star } from "lucide-react";
 import { Toaster } from "@/components/ui/sonner";
 import { toast } from "sonner";
 import getLogger from "@/lib/logger";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
+import prisma from "@/lib/prisma";
+import { getSession } from "next-auth/react";
 
 const logger = getLogger("EditReviewPage");
 
@@ -20,35 +23,74 @@ interface Review {
   screenName: string;
 }
 
-export default function EditReviewPage() {
+interface EditReviewPageProps {
+  review: Review;
+}
+
+export const getServerSideProps: GetServerSideProps<
+  EditReviewPageProps
+> = async (context: GetServerSidePropsContext) => {
+  const session = await getSession(context);
+  const { id } = context.params as { id: string };
+
+  if (!id) {
+    return {
+      notFound: true,
+    };
+  }
+
+  try {
+    const review = await prisma.seatReview.findUnique({
+      where: {
+        id: parseInt(id),
+        user_id: parseInt(session!.user!.id, 10),
+      },
+      include: {
+        users: {
+          select: {
+            name: true,
+          },
+        },
+        screens: {
+          include: {
+            theaters: true,
+          },
+        },
+      },
+    });
+
+    if (!review) {
+      return {
+        notFound: true,
+      };
+    }
+    const formattedReviews: Review = {
+      id: review.id,
+      user: { name: review.users.name! },
+      seatNumber: review.seat_name,
+      rating: review.rating,
+      review: review.review,
+      theaterName: review.screens.theaters.name,
+      screenName: review.screens.name,
+    };
+
+    return {
+      props: {
+        review: formattedReviews,
+      },
+    };
+  } catch (error) {
+    logger.error("Error fetching review:", error);
+    return {
+      notFound: true,
+    };
+  }
+};
+
+export default function EditReviewPage({ review }: EditReviewPageProps) {
   const router = useRouter();
-  const id = useSearchParams().get('id');
-  const [review, setReview] = useState<Review | null>(null);
   const [editedReview, setEditedReview] = useState("");
   const [editedRating, setEditedRating] = useState(0);
-
-  useEffect(() => {
-    if (id) {
-      fetchReview();
-    }
-  }, [id]);
-
-  const fetchReview = async () => {
-    try {
-      const response = await fetch(`/api/reviews/${id}`);
-      if (response.ok) {
-        const data = await response.json();
-        setReview(data);
-        setEditedReview(data.review);
-        setEditedRating(data.rating);
-      } else {
-        toast.error("レビューの取得に失敗しました");
-      }
-    } catch (error) {
-      logger.debug(error);
-      toast.error("レビューの取得に失敗しました");
-    }
-  };
 
   const handleSave = async () => {
     try {
@@ -98,8 +140,9 @@ export default function EditReviewPage() {
     router.back();
   };
 
+  console.log(review);
   if (!review) {
-    return <div>Loading...</div>;
+    return <div>レビューが見つかりません</div>;
   }
 
   return (
