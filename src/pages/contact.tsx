c1eaab6c626d0c572b786c7bb9126ef651b84c78
yasudaProduct <yasu.private.ct@gmail.com>
2024-11-09 20:39:32 +0900

    contactPage追加 notion

---
+++ src/pages/contact.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function ContactPage() {
+  return (
+    <div className="container mx-auto">
+      <iframe
+        style={{ border: "none", width: "100%" }}
+        height="400px"
+        src="https://www.noway-form.com/ja/f/beb1df70-2e2a-40af-bf71-8ea3cc6a2236/embed"
+      />
+    </div>
+  );
+}
