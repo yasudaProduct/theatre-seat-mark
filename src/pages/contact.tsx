5d2fe3bd53a89ac41d3bc0e0f085f373473cbb17
yasudaProduct <yasu.private.ct@gmail.com>
2024-11-10 19:06:11 +0900

    デザイン修正

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,13 +1,22 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import React from "react";
 
 export default function ContactPage() {
   return (
-    <div className="container mx-auto">
-      <iframe
-        style={{ border: "none", width: "100%" }}
-        height="830px"
-        src="https://www.noway-form.com/ja/f/beb1df70-2e2a-40af-bf71-8ea3cc6a2236/embed"
-      />
+    <div className="container mx-auto mt-10">
+      <Card>
+        <CardHeader>
+          <CardTitle></CardTitle>
+        </CardHeader>
+        <CardContent>
+          <iframe
+            className="overflow-hidden"
+            style={{ border: "none", width: "100%" }}
+            height="750px"
+            src="https://www.noway-form.com/ja/f/beb1df70-2e2a-40af-bf71-8ea3cc6a2236/embed"
+          />
+        </CardContent>
+      </Card>
     </div>
   );
 }
