15be2f33d064cc1c75d09d74d96168d16c3863ae
yasudaProduct <yasu.private.ct@gmail.com>
2024-12-21 21:26:35 +0900

    :fire:

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -20,9 +20,6 @@ export const authOptions: AuthOptions = {
   ],
   callbacks: {
     async session({ session, user }) {
-      console.log("callbacks");
-      console.log("session:", session);
-      console.log("user:", user);
       if (session?.user) {
         session.user.id = user.id;
         session.user.aliasId = user.aliasId;
