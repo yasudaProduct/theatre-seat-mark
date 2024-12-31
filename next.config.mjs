c03cf03dba6aaf588cbfb675c1814c39c338c01f
yasudaProduct <yasu.private.ct@gmail.com>
2024-12-31 15:13:06 +0900

    :sparkles: メンテナンス画面追加

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,24 @@
+import process from "process";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["lh3.googleusercontent.com"],
+  },
+  async redirects() {
+    const maintenanceMode = process.env.MAINTENANCE_MODE;
+    console.log(maintenanceMode);
+    if (maintenanceMode === "true") {
+      return [
+        {
+          source: "/((?!maintenance.html$).*$)",
+          destination: "/maintenance.html",
+          permanent: false,
+        },
+      ];
+    }
+    return [];
   },
 };
 
