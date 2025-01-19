ffa8aa3784578c99ff7924fff87e0913aa19aeff
yasudaProduct <yasu.private.ct@gmail.com>
2025-01-19 19:32:41 +0900

    :lipstick: デザイン修正 #32

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function ContactPage() {
   return (
-    <div className="container mx-auto mt-10">
+    <div className="container mx-auto mt-10 mb-10">
       <Card>
         <CardHeader>
           <CardTitle></CardTitle>
@@ -12,7 +12,7 @@ export default function ContactPage() {
           <iframe
             className="overflow-hidden"
             style={{ border: "none", width: "100%" }}
-            height="750px"
+            height="800px"
             src="https://www.noway-form.com/ja/f/beb1df70-2e2a-40af-bf71-8ea3cc6a2236/embed"
           />
         </CardContent>
