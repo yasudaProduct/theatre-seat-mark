32dd647a7e8e97e7ce5d8d92a574d810f030982d
yasudaProduct <yasu.private.ct@gmail.com>
2025-01-20 21:41:26 +0900

    :lipstick: 微修正

--- src/pages/[aliasId].tsx
+++ src/pages/[aliasId].tsx
@@ -157,14 +157,18 @@ export default function UserProfile({ user, isOwnProfile }: UserProfileProps) {
             />
             <AvatarFallback>{user.name?.charAt(0) || "U"}</AvatarFallback>
           </Avatar>
-          <div className="space-y-1">
-            <div>
-              <span className="text-2xl">{user.name}</span>
-              <span className="text-xs text-gray-500 ml-5">{user.aliasId}</span>
+          <div>
+            <div className="space-y-1">
+              <div>
+                <span className="text-2xl">{user.name}</span>
+                <span className="text-xs text-gray-500 ml-5">
+                  {user.aliasId}
+                </span>
+              </div>
             </div>
             {isOwnProfile && (
               <Link legacyBehavior href="/settings">
-                <a className="my-3 display: block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                <a className="my-3 block w-40 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                   プロフィール設定
                 </a>
               </Link>
