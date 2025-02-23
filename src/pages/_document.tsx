d81530d4f783be969285ff18cbdf832f03d2ea5e
yasudaProduct <yasu.private.ct@gmail.com>
2025-02-23 16:55:57 +0900

    :lipstick: OGP

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,25 @@ class MyDocument extends Document {
       <Html lang="ja">
         <Head>
           <title>シネポジ</title>
+          {/* OGP Basic */}
+          <meta
+            property="og:title"
+            content="シネポジ - 映画館の座席レビューサービス"
+          />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://sineposi.vercel.app/" />
+          <meta property="og:image" content="/images/ogp.png" />
+
+          {/* OGP Additional */}
+          <meta property="og:site_name" content="シネポジ" />
+          <meta
+            property="og:description"
+            content="映画館の座席レビューを登録し、あなたの好みの座席を記録しよう。"
+          />
+
+          {/* Twitter Card */}
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@sineposi" />
         </Head>
         <body>
           <Main />
